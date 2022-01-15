08bae1ed41651697ab72d73c63f4fef2218a5a2c
francisldn <fnhh04@gmail.com>
2022-01-15 19:56:16 +0000

    add contract address and abi

---
+++ src/contracts/config.js
@@ -0,0 +1,4 @@
+
+  export const disputeNFTMarket = '0x0b344f832c3e21d248696c4a78b0b148c650401c'
+  export const disputeNFT = '0xa585c9a8125a0ad4cd2063d9f1006734b5cb81f6'
+  export const escrow = '0x6a918d2b923ee6fe70cc514c6a7189dee62dd412'
